28e5b0db9ac1e4ab79ef30e2f05df35dab7ae33b
HerrLauch <keuwel-neuwel@gmx.de>
2026-07-13 11:19:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,25 @@
-```javascript
+const zielDatum = new Date("2026-08-05T10:55:00");
 
-alert("JavaScript läuft!");
-
-// Zielzeit des Abflugs
-const zielDatum = new Date("August 5, 2026 10:55:00").getTime();
-
-
-function countdown(){
-
-    const jetzt = new Date().getTime();
-
+function countdown() {
+    const jetzt = new Date();
     const unterschied = zielDatum - jetzt;
 
-
-    // Wenn der Countdown abgelaufen ist
-    if(unterschied <= 0){
-
+    if (unterschied <= 0) {
         document.getElementById("countdown").innerHTML = `
             <div class="time-box">
                 <div class="number">✈️</div>
-                <div class="label">Es geht los!</div>
+                <div class="label">Der Urlaub beginnt!</div>
             </div>
         `;
-
         return;
     }
 
+    const tage = Math.floor(unterschied / (1000 * 60 * 60 * 24));
+    const stunden = Math.floor((unterschied / (1000 * 60 * 60)) % 24);
+    const minuten = Math.floor((unterschied / (1000 * 60)) % 60);
+    const sekunden = Math.floor((unterschied / 1000) % 60);
 
-    // Berechnung der Zeit
-    const tage = Math.floor(
-        unterschied / (1000 * 60 * 60 * 24)
-    );
-
-    const stunden = Math.floor(
-        (unterschied % (1000 * 60 * 60 * 24)) /
-        (1000 * 60 * 60)
-    );
-
-    const minuten = Math.floor(
-        (unterschied % (1000 * 60 * 60)) /
-        (1000 * 60)
-    );
-
-    const sekunden = Math.floor(
-        (unterschied % (1000 * 60)) /
-        1000
-    );
-
-
-    // Anzeige auf der Webseite
     document.getElementById("countdown").innerHTML = `
-
         <div class="time-box">
             <div class="number">${tage}</div>
             <div class="label">Tage</div>
@@ -70,14 +39,8 @@ function countdown(){
             <div class="number">${sekunden}</div>
             <div class="label">Sekunden</div>
         </div>
-
     `;
 }
 
-
-// Sofort starten
 countdown();
-
-// Jede Sekunde aktualisieren
-setInterval(countdown,1000);
-```
+setInterval(countdown, 1000);
