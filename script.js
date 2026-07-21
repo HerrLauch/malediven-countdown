44ebbeaef3f3fdedd0fd9894ce3ff6347b8bf130
HerrLauch <keuwel-neuwel@gmx.de>
2026-07-21 15:00:35 +0200

    Add end vacation date to script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const zielDatum = new Date("2026-08-05T04:00:00");
 
 const ankunftAngaga = new Date("2026-08-06T08:00:00");
-
+const endeUrlaub = new Date("2026-08-20T08:00:00");
 let reiseGestartet = false;
 function countdown() {
 
